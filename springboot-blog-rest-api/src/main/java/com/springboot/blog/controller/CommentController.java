aa8d0a0a9ce1c3679ffd65c4f973bf7ca82253e3
Jordan Hackworth <jhackworth00@outlook.com>
2022-11-27 22:16:52 -0600

    validation constraint @entitys for comments

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.service.CommentService;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class CommentController {
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId")long postId,
-                                                    @RequestBody CommentDto commentDto) {
+                                                    @Valid @RequestBody CommentDto commentDto) {
        return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -39,7 +40,7 @@ public class CommentController {
     public ResponseEntity<CommentDto> updateComment(
                                     @PathVariable(value = "postId")Long postId,
                                     @PathVariable(value = "id")Long commentId,
-                                    @RequestBody CommentDto commentDto) {
+                                    @Valid @RequestBody CommentDto commentDto) {
         CommentDto updatedComment = commentService.updateComment(commentId, postId, commentDto);
         return new ResponseEntity<>(updatedComment, HttpStatus.OK);
     }
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,11 +1,23 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private Long id;
+    // name should not be null or empty
+    @NotEmpty(message = "Name cannot be empty or Null")
     private String name;
+    // email field validation
+    // email should not be null or empty
+    @NotEmpty(message = "Email cannot be empty or Null")
+    @Email(message = "Please provide a valid email")
     private String email;
+    // comment should not be null or empty
+    @NotEmpty(message = "Comment cannot be empty or Null")
+    @Size(min = 2, max = 100, message = "Comment should have at least 2 characters and at most 100 characters")
     private String body;
 }
