631f49d06dd31d45ab4553e0c05edea123e2e645
Jordan Hackworth <jhackworth00@outlook.com>
2022-11-27 20:37:02 -0600

    imported modelmapper + refactored private impl methods to use it

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,10 +1,18 @@
 package com.springboot.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBlogRestApiApplication {
+
+	//model mapper is a library that maps one object to another object
+	@Bean //this will make the model mapper available to the entire application
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,7 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,9 +22,12 @@ public class PostServiceImpl implements PostService {
 
     private PostRepository postRepository;
 
+    private ModelMapper mapper;
+
     @Autowired // this indicates that Spring should inject an instance of PostRepository here.
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, ModelMapper mapper) {
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -99,21 +103,23 @@ public class PostServiceImpl implements PostService {
 
     //convert Entity into DTO
     private PostDto mapToDTO(Post post) {
-       PostDto postDto = new PostDto();
-       postDto.setId(post.getId());
-       postDto.setTitle(post.getTitle());
-       postDto.setDescription(post.getDescription());
-       postDto.setContent(post.getContent());
+        PostDto postDto = mapper.map(post, PostDto.class);
+//       PostDto postDto = new PostDto();
+//       postDto.setId(post.getId());
+//       postDto.setTitle(post.getTitle());
+//       postDto.setDescription(post.getDescription());
+//       postDto.setContent(post.getContent());
        return postDto;
     }
 
     //convert DTO into Entity
     private Post mapToEntity(PostDto postDto) {
-        Post post = new Post();
-        post.setId(postDto.getId());
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        Post post = mapper.map(postDto, Post.class);
+//        Post post = new Post();
+//        post.setId(postDto.getId());
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
         return post;
     }
 }
